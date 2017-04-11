5bebc8aab250474cba89511ee76e1632b1c9ebc0
wxl <lzcrwxl@163.com>
2017-04-11 21:26:00 +0800

    f

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App'
+import goods from './components/goods/goods';
+import ratings from './components/ratings/ratings.vue';
+import seller from './components/seller/seller.vue';
+import VueResource from 'vue-resource';
+import 'common/stylus/index.styl';
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+///* eslint-disable no-new */
+//new Vue({
+//  el: 'body',
+//  components: { App }
+//})
+
+let app=Vue.extend(App);
+
+let router=new VueRouter({
+  linkActiveClass:'active'
+});
+
+router.map({
+  '/goods':{
+    component:goods
+  },
+  '/ratings':{
+    component:ratings
+  },
+  '/seller':{
+    component:seller
+  },
+})
+
+router.start(app,'#app')
+router.go('/goods')
